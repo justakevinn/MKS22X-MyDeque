5d1294f87b22491574b8aa9ab37ee065af3c5584
Kevin <kzong@stuy.edu>
2019-03-25 04:01:13 -0400

    Finished AddFirst

--- MyDeque.java
+++ MyDeque.java
@@ -27,38 +27,80 @@ public class MyDeque<E>{
 
   public String toString(){
     String s = "{";
-    for (int x = start; x < size; x++){
-      s += data[x] + " ";
+    if (start <= end){
+      for (int x = start; x <= end; x++){
+        if( data[x]!= null ){
+          s += data[x] + " ";
+        }
+      }
+      System.out.println("a");
     }
-    for (int x = 0; x < end; x++){
-      s+= data[x] + " ";
+    else{
+      for (int x = start; x < data.length; x++){
+        if( data[x]!= null ){
+          s += data[x] + " ";
+        }
+      }
+      for (int x = 0; x <= end; x++){
+        if( data[x]!= null ){
+          s += data[x] + " ";
+        }
+      }
+      System.out.println("b");
     }
     s += "}";
     return s;
-  }*/
+  }
 
   public void addFirst(E element){
-    resize();
-    data[size] = element;
-    start = size;
-    size ++;
+    if(element == null){
+       throw new NullPointerException();
+      }
+    resize(); //There are two cases for adding first:
+    if(start == 0){
+          data[data.length - 1] = element;
+          start = data.length - 1;
+        }
+    else{
+       data[start - 1] = element;
+        start--;
+      }
+      size+= 1;
+    //System.out.println(size);
+    //System.out.println(data.length);
   }
+
+
+  
+
 
   private void resize(){
     if (size == data.length){
       E[] d = (E[])new Object[size*2];
-      for (int x = 0; x < data.length; x++){
-        d[x] = data[x];
-      }
-      data = d;
+      for (int i = 0; i < data.length; i++){
+        if (start <= end){
+          for (int x = start; x <= end; x++){
+            d[i] = data[x];
+          }
+        }
+        else{
+          for (int x = start; x < data.length; x++){
+            d[i] = data[x];
+          }
+          for (int x = 0; x <= end; x++){
+            d[i] = data[x];
+          }
+        }
     }
+    data = d;
   }
+}
 
 
   public void addLast(E element){
     resize();
     data[size] = element;
-    last = size;
+    end = size;
     size++;
   }
 
@@ -66,5 +108,23 @@ public class MyDeque<E>{
   /*  public E removeFirst(){ }
   public E removeLast(){ }
   public E getFirst(){ }
-  public E getLast(){ }*/
+  public E getLast(){ }
+  */
+
+  public static void main (String[] args){
+    MyDeque test = new MyDeque(4);
+    test.addFirst(1);
+    System.out.println(test.toString());
+    test.addFirst(2);
+    System.out.println(test.toString());
+    test.addFirst(3);
+    System.out.println(test.toString());
+    test.addFirst(4);
+    System.out.println(test.toString());
+    test.addFirst(5);
+    System.out.println(test.toString());
+
+  }
+
+
 }
